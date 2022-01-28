4bdddb2528bd91c8a58b3f1229decf48e1cc50e1
Michael Huang <michaelyhuang23@gmail.com>
2022-01-27 22:29:02 -0500

    gss5 debuged

--- GSS1.cpp
+++ GSS1.cpp
@@ -8,19 +8,28 @@ typedef long long llong;
 #define print(x) cout<<x<<endl
 #define inf 1000000000000000LL
 
-struct Node{
+struct NodeN{
 	llong sum;
 	llong p, s, a;
-	Node(){sum = -inf; p = -inf; s = -inf; a = -inf;}
-	Node(llong sum_, llong p_, llong s_, llong a_){
-		sum = sum_;
-		p = p_;
-		s = s_;
-		a = a_;
+	NodeN(){sum = -inf; p = -inf; s = -inf; a = -inf;}
+	NodeN(llong val){
+		sum = val;
+		p = val;
+		s = val;
+		a = val;
+	}
+	NodeN merge(NodeN r){
+		NodeN m;
+		m.sum = sum+r.sum;
+		m.p = max(p, sum+r.p);
+		m.s = max(r.s, r.sum+s);
+		m.a = max({a,r.a,s+r.p});
+		return m;
 	}
 };
+template <class N>
 struct segtree{
-	vector<Node> tree;
+	vector<N> tree;
 	llong n;
 	llong pow_up(llong n){
 		for(llong i=0;i<32;i++) if((1<<i)>=n) return 1<<i;
@@ -28,37 +37,26 @@ struct segtree{
 	}
 	segtree(vector<llong>& arr, llong n_){
 		n = pow_up(n_);
-		tree = vector<Node>(n*2);
+		tree = vector<N>(n*2);
 		for(llong i=0;i<n_;i++){
-			tree[n+i].sum = arr[i];
-			tree[n+i].p = arr[i];
-			tree[n+i].s = arr[i];
-			tree[n+i].a = arr[i];
+			tree[n+i] = N(arr[i]);
 		}
 		build();
 	}
-	Node merge(Node l, Node r){
-		Node m;
-		m.sum = l.sum+r.sum;
-		m.p = max(l.p, l.sum+r.p);
-		m.s = max(r.s, r.sum+l.s);
-		m.a = max({l.a,r.a,l.s + r.p});
-		return m;
-	}
 	void pull_up(llong i){
-		tree[i] = merge(tree[i*2], tree[i*2+1]);
+		tree[i] = tree[i*2].merge(tree[i*2+1]);
 	}
 	void build(){
 		for(llong i=n-1;i>=1;i--) pull_up(i);
 	}
 	llong query(llong l, llong r){
-		Node ll, rr;
+		N ll, rr;
 		for(l+=n, r+=n; l<=r; l/=2, r/=2){
-			if(l&1) ll = merge(ll,tree[l++]);
-			if(!(r&1)) rr = merge(tree[r--], rr);
+			if(l&1) ll = ll.merge(tree[l++]);
+			if(!(r&1)) rr = tree[r--].merge(rr);
 			if(l == r) break;
 		}
-		Node mm = merge(ll,rr);
+		N mm = ll.merge(rr);
 		return mm.a;
 	}
 };
@@ -70,7 +68,7 @@ int main(){
 	llong n; cin >> n;
 	vector<llong> arr(n);
 	for(llong i=0;i<n;i++) cin >> arr[i];
-	segtree tr(arr, n);
+	segtree<NodeN> tr(arr, n);
 	llong m; cin >> m;
 	for(llong i=0;i<m;i++){
 		llong x, y; cin >> x >> y;
