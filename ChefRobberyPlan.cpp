e6c922f2dbda276c191f031b5b0bec827a81b8ea
Michael Huang <michaelyhuang23@gmail.com>
2022-02-18 00:05:21 -0500

    buggy robbery

--- ChefRobberyPlan.cpp
+++ ChefRobberyPlan.cpp
@@ -2,36 +2,66 @@
 
 using namespace std;
 
-
+typedef long long llong;
+#define mt make_tuple
+#define mp make_pair
 #define pb push_back
+#define print(x) cout<<x<<endl
+#define inf 1000000000000LL
 
+llong N, K, W;
+vector<llong> values;
 
-int N, K, W;
-vector<int> values;
-
-vector<int> solve(int k, int a, int b){ // a, b are weights
+vector<llong> solve(llong k, llong a, llong b){ // a, b are weights
 	if(k == 1){
-		vector<int> ret(b-a+1);
-		for(int w=a;w<=b;w++) ret[w-a]=values[w];
+		vector<llong> ret(b-a+1, -inf);
+		for(llong w=a;w<=b;w++) ret[w-a]=values[w];
+		//deb(mt(k,a,b))
+		//deb(ret)
 		return ret;
 	}
 	if(k%2==0){
-		int st = max(0, a/2-N); // maybe add 1 to both ranges?
-		int ed = min(b/2+((b%2==1)?1:0)+N, W);
-		vector<int> sol = solve(k/2, st, ed); 
-		for(int w = a; w<=b; w++){
-			
+		llong st = max(0LL, (a-N)/2); // maybe add 1 to both ranges?
+		llong ed = min((b+N)/2+(((b+N)%2==1)?1:0), W);
+		vector<llong> sol = solve(k/2, st, ed); 
+		vector<llong> ret(b-a+1, -inf);
+		for(llong w=a; w<=b; w++){
+			for(llong i=0; i<=N/2; i++){
+				llong ll = w/2 - i;
+				if(ll<0) continue;
+				llong rr = w/2 + ((w%2)?1:0) + i;
+				if(rr>W) continue;
+				// assert(ll + rr == w);
+				// assert(ll >= st);
+				// assert(rr <= ed);
+				//deb(mt(ll,rr,sol[ll-st]+sol[rr-st]))
+				ret[w-a] = max(ret[w-a], sol[ll-st] + sol[rr-st]);
+			}
 		}
+		//deb(mt(k,a,b))
+		//deb(ret)
+		return ret;
 	}else{
-
+		vector<llong> ret(b-a+1, -inf);
+		vector<llong> sols = solve(k-1, 0, b-1);
+		for(llong w=a; w<=b; w++){
+			for(llong i=1; i<=N; i++){
+				//deb(mt(w,sols[w-i],values[i]))
+				ret[w-a] = max(ret[w-a], sols[w-i] + values[i]);
+			}
+		}
+		//deb(mt(k,a,b))
+		//deb(ret)
+		return ret;
 	}
 }
 
 void solution(){
 	cin >> N >> K >> W;
-	values = vector<int>(N);
-	for(int i=0;i<N;i++) cin >> values[i];
-	solve(K, W, W);
+	values = vector<llong>(N*2,-inf);
+	for(llong i=1;i<=N;i++) cin >> values[i];
+	vector<llong> sol = solve(K, W, W);
+	print(sol[0]);
 }
 
 int main(){
@@ -39,3 +69,5 @@ int main(){
 	cin.tie(0);
 	solution();
 }
+
+
