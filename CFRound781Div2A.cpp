1f1766f31a6185eb07c3b75b7c12469518edbc81
Michael Huang <michaelyhuang23@gmail.com>
2022-04-08 11:07:07 -0400

    move

---
+++ CFRound781Div2A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+void solve(){
+	int n; cin >> n;
+	assert(n>=4);
+
+	cout<<n-3<<" "<<1<<" "<<1<<" "<<1<<endl;
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int tt; cin >> tt;
+	while(tt--) solve();
+}
